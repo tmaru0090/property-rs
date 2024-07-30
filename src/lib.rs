a8452ae92e48a59cb797ebcb0cb379e84189a894
tanukimaru0090 <tanusan1015@gmail.com>
2024-07-30 22:33:13 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 
 use proc_macro::TokenStream;
 use quote::{quote,format_ident};
-use syn::{Type,parse_macro_input, DeriveInput, Data, Fields,Meta};
+use syn::{Type,parse_macro_input, DeriveInput, Data, Fields,Meta,Ident,Token};
 use syn::spanned::Spanned;
-
+use syn::parse::ParseStream;
 /*
 #[proc_macro_derive(Property, attributes(getter, setter))]
 pub fn property_derive(input: TokenStream) -> TokenStream {
@@ -118,6 +118,7 @@ pub fn property_derive(input: TokenStream) -> TokenStream {
 
 
 
+
 #[proc_macro_derive(Property, attributes(property))]
 pub fn derive_property(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
@@ -131,31 +132,60 @@ pub fn derive_property(input: TokenStream) -> TokenStream {
                 let getter_name = field_name;
                 let setter_name = format_ident!("set_{}", field_name.as_ref().unwrap());
 
-                // Determine if the field type is a reference
-                let is_reference = matches!(field_type, syn::Type::Reference(_));
-                let getter = if is_reference {
-                    // Field type is a reference
-                    quote! {
-                        pub fn #getter_name(&self) -> &#field_type {
-                            &self.#field_name
+                let mut generate_getter = false;
+                let mut generate_setter = false;
+
+                for attr in &f.attrs {
+                    if attr.path().is_ident("property") {
+                        attr.parse_args_with(|input: ParseStream| {
+                            while !input.is_empty() {
+                                let ident: Ident = input.parse()?;
+                                if ident == "get" {
+                                    generate_getter = true;
+                                } else if ident == "set" {
+                                    generate_setter = true;
+                                }
+                                if !input.is_empty() {
+                                    input.parse::<Token![,]>()?;
+                                }
+                            }
+                            Ok(())
+                        }).unwrap();
+                    }
+                }
+
+                let getter = if generate_getter {
+                    if matches!(field_type, syn::Type::Reference(_)) {
+                        quote! {
+                            pub fn #getter_name(&self) -> &#field_type {
+                                &self.#field_name
+                            }
+                        }
+                    } else {
+                        quote! {
+                            pub fn #getter_name(&self) -> #field_type {
+                                self.#field_name.clone()
+                            }
                         }
                     }
                 } else {
-                    // Field type is not a reference
+                    quote! {}
+                };
+
+                let setter = if generate_setter {
                     quote! {
-                        pub fn #getter_name(&self) -> #field_type {
-                            self.#field_name.clone()
+                        pub fn #setter_name(&mut self, value: #field_type) {
+                            self.#field_name = value;
                         }
                     }
+                } else {
+                    quote! {}
                 };
 
                 quote! {
                     impl #name {
                         #getter
-
-                        pub fn #setter_name(&mut self, value: #field_type) {
-                            self.#field_name = value;
-                        }
+                        #setter
                     }
                 }
             })
