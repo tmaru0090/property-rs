d2f06fa4170b55fba1db71981d0a8a7227ab9029
tanukimaru0090 <tanusan1015@gmail.com>
2024-07-30 13:05:54 +0900

    Update

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Data, Fields};
+use syn::spanned::Spanned;
+
+
+#[proc_macro_derive(Property, attributes(getter, setter))]
+pub fn property_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+
+    let expanded = match input.data {
+        syn::Data::Struct(data) => {
+            let getters_and_setters = data.fields.iter().map(|field| {
+                let field_name = &field.ident;
+                let field_type = &field.ty;
+                let mut getter = None;
+                let mut setter = None;
+
+                for attr in &field.attrs {
+                    if attr.path().is_ident("getter") {
+                        getter = Some(quote! {
+                            pub fn #field_name(&self) -> &#field_type {
+                                &self.#field_name
+                            }
+                        });
+                    } else if attr.path().is_ident("setter") {
+                        let setter_name = syn::Ident::new(&format!("set_{}", field_name.as_ref().unwrap()), field_name.span());
+                        setter = Some(quote! {
+                            pub fn #setter_name(&mut self, value: #field_type) {
+                                self.#field_name = value;
+                            }
+                        });
+                    }
+                }
+
+                quote! {
+                    #getter
+                    #setter
+                }
+            });
+
+            quote! {
+                impl #name {
+                    #(#getters_and_setters)*
+                }
+            }
+        }
+        _ => quote! {},
+    };
+
+    TokenStream::from(expanded)
+}
