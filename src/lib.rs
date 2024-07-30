6c02bdf9eab89647343412e1ee72a59242e4997e
tanukimaru0090 <tanusan1015@gmail.com>
2024-07-30 22:26:43 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote,format_ident};
 use syn::{Type,parse_macro_input, DeriveInput, Data, Fields,Meta};
 use syn::spanned::Spanned;
 
@@ -117,72 +117,57 @@ pub fn property_derive(input: TokenStream) -> TokenStream {
 
 
 
+
 #[proc_macro_derive(Property, attributes(property))]
-pub fn property_derive(input: TokenStream) -> TokenStream {
+pub fn derive_property(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let name = input.ident;
+    let name = &input.ident;
 
-    let expanded = match input.data {
-        syn::Data::Struct(data) => {
-            let getters_and_setters = data.fields.iter().filter_map(|field| {
-                let field_name = field.ident.as_ref()?;
-                let field_type = &field.ty;
-                let mut getter = None;
-                let mut setter = None;
+    let expanded = if let Data::Struct(data) = &input.data {
+        let fields = if let Fields::Named(fields) = &data.fields {
+            fields.named.iter().map(|f| {
+                let field_name = &f.ident;
+                let field_type = &f.ty;
+                let getter_name = field_name;
+                let setter_name = format_ident!("set_{}", field_name.as_ref().unwrap());
 
-                for attr in &field.attrs {
-                    if attr.path().is_ident("property") {
-                        if let Ok(_) = attr.parse_nested_meta(|meta| {
-                            if let Ok(meta) = meta.value()?.parse::<Meta>() {
-                                if let Meta::Path(path) = meta {
-                                    if path.is_ident("get") {
-                                        let is_reference = matches!(field_type, syn::Type::Reference(_));
-                                        getter = Some(if is_reference {
-                                            quote! {
-                                                pub fn #field_name(&self) -> &#field_type {
-                                                    &self.#field_name
-                                                }
-                                            }
-                                        } else {
-                                            quote! {
-                                                pub fn #field_name(&self) -> #field_type {
-                                                    self.#field_name.clone()
-                                                }
-                                            }
-                                        });
-                                    } else if path.is_ident("set") {
-                                        let setter_name = syn::Ident::new(&format!("set_{}", field_name), field_name.span());
-                                        setter = Some(quote! {
-                                            pub fn #setter_name(&mut self, value: #field_type) {
-                                                self.#field_name = value;
-                                            }
-                                        });
-                                    }
-                                }
-                            }
-                            Ok(())
-                        }) {
-                            // 属性の解析に成功した場合の処理
-                        } else {
-                            // 属性の解析に失敗した場合の処理
-                            return None;
+                // Determine if the field type is a reference
+                let is_reference = matches!(field_type, syn::Type::Reference(_));
+                let getter = if is_reference {
+                    // Field type is a reference
+                    quote! {
+                        pub fn #getter_name(&self) -> &#field_type {
+                            &self.#field_name
+                        }
+                    }
+                } else {
+                    // Field type is not a reference
+                    quote! {
+                        pub fn #getter_name(&self) -> #field_type {
+                            self.#field_name.clone()
+                        }
+                    }
+                };
+
+                quote! {
+                    impl #name {
+                        #getter
+
+                        pub fn #setter_name(&mut self, value: #field_type) {
+                            self.#field_name = value;
                         }
                     }
                 }
+            })
+        } else {
+            panic!("Property macro only supports named fields");
+        };
 
-                Some(quote! {
-                    #getter
-                    #setter
-                })
-            }).collect::<Vec<_>>();
-
-            quote! {
-                impl #name {
-                    #(#getters_and_setters)*
-                }
-            }
+        quote! {
+            #(#fields)*
         }
-        _ => quote! {},
+    } else {
+        panic!("Property macro only supports structs");
     };
 
     TokenStream::from(expanded)
